22c6e425a04ec5445fe1b1b85c195f1637a35502
vikalchari <vikalchari@gmail.com>
2026-02-18 13:15:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -27,3 +27,23 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
+const flowerContainer = document.querySelector(".name-wrapper");
+
+function createFlower() {
+  const flower = document.createElement("div");
+  flower.classList.add("flower");
+
+  const flowers = ["🌸", "🌺", "🌼", "🌷"];
+  flower.innerText = flowers[Math.floor(Math.random() * flowers.length)];
+
+  flower.style.left = Math.random() * 100 + "%";
+  flower.style.animationDuration = (3 + Math.random() * 3) + "s";
+
+  flowerContainer.appendChild(flower);
+
+  setTimeout(() => {
+    flower.remove();
+  }, 6000);
+}
+
+setInterval(createFlower, 400);
